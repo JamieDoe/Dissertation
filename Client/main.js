f7daff4a0c36216db19c3758126cb38320dfd8a1
UP953058 <UP953068@myport.ac.uk>
2023-04-18 10:53:16 +0100

    Added navbar. Started styling navbar. Added classes/ids to new elements

--- Client/main.js
+++ Client/main.js
@@ -1,39 +1,88 @@
 // This is the main.js file for the client side of the application
 
-const fetch = require('node-fetch');
-
-
 // get the body element.
 const body = document.querySelector('body');
 
+// create main content, navbar ect.
+function allPageContent() {
+
+    const navContainer = document.createElement('div');
+    const navList = document.createElement('ul');
+    const navItem1 = document.createElement('li');
+    const navItem2 = document.createElement('li');
+    const navItem3 = document.createElement('li');
+    const navItem4 = document.createElement('li');
+    const navItem5 = document.createElement('li');
+
+    navContainer.setAttribute('id', 'navContainer');
+    navList.setAttribute('id', 'navList');
+    navItem1.setAttribute('class', 'navItem');
+    navItem2.setAttribute('class', 'navItem');
+    navItem3.setAttribute('class', 'navItem');
+    navItem4.setAttribute('class', 'navItem');
+    navItem5.setAttribute('class', 'navItem');
+
+
+    body.appendChild(navContainer);
+    navContainer.appendChild(navList);
+    navList.appendChild(navItem1);
+    navList.appendChild(navItem2);
+    navList.appendChild(navItem3);
+    navList.appendChild(navItem4);
+    navList.appendChild(navItem5);
+
+    // add content to the nav items.
+    navItem1.innerHTML = 'Settings';
+    navItem2.innerHTML = 'History';
+    navItem3.innerHTML = 'Home';
+    navItem4.innerHTML = 'Videos';
+    navItem5.innerHTML = 'Profile';
+
+        // for each element with the class 'navItem', append to the navList.
+        document.querySelectorAll('.navItem').forEach(item => {
+            navList.appendChild(item);
+        });
+
+};
+
 // create the main page content.
-function pageContent() {
+function login() {
 
     console.log('the page has loaded');
 
     // create the div that will hold the buttons & create the buttons.
-    const logBtnContainer = document.createElement('div', {id: 'logBtnContainer'});
-    const loginButton = document.createElement('button', {id: 'loginButton'});
-    const logoutButton = document.createElement('button', {id: 'logoutButton'});
-    const profileButton = document.createElement('button', {id: 'profileButton'});
+    const logBtnContainer = document.createElement('div');
+    const loginButton = document.createElement('button');
+    const logoutButton = document.createElement('button');
+    const profileButton = document.createElement('button');
     
+    logBtnContainer.setAttribute('id', 'logBtnContainer');
+    loginButton.setAttribute('class', 'logBtns');
+    logoutButton.setAttribute('class', 'logBtns');
+    profileButton.setAttribute('class', 'logBtns');
+
+    // add content to the buttons.
     loginButton.innerHTML = 'Login';
     logoutButton.innerHTML = 'Logout';
     profileButton.innerHTML = 'Profile';
 
     // append div to body, append the buttons to the div.
-    body.appendChild(logBtnContainer);
+
     logBtnContainer.appendChild(loginButton);
     logBtnContainer.appendChild(logoutButton);
     logBtnContainer.appendChild(profileButton);
+
+    body.appendChild(logBtnContainer);
 
     // add event listeners to the buttons.
     loginButton.addEventListener('click', () => {
         window.location.href = 'http://localhost:8080/login';
     });
+
     logoutButton.addEventListener('click', () => {
         window.location.href = 'http://localhost:8080/logout';
     });
+
     profileButton.addEventListener('click', () => {
         userProfile();
     });
@@ -72,7 +121,8 @@ function userProfile() {
 };
 
 function init() {
-    pageContent();
+    allPageContent();
+    login();
 };
 
 window.addEventListener('load', init);
